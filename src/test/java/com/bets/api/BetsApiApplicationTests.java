ebdafad843a3cf0d9650a0ffdd6885a4bfb23f6e
ruanvalente <ruanopensuselinux@gmail.com>
2022-07-20 18:08:41 -0300

    first commit

---
+++ src/test/java/com/bets/api/BetsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bets.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
