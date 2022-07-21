e5ae015b4b60a491e325e730bdc2426068e1d430
ruanvalente <ruanopensuselinux@gmail.com>
2022-07-21 12:39:09 -0300

    feat: add close #7; punter repository

--- src/main/java/com/bets/api/respository/PunterRepository.java
+++ src/main/java/com/bets/api/respository/PunterRepository.java
@@ -2,7 +2,9 @@ package com.bets.api.respository;
 
 import com.bets.api.entities.Punter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface PunterRepository extends JpaRepository<Punter, Long> {
+@Repository
+public interface PunterRepository extends JpaRepository<Punter, Integer> {
 }
 
