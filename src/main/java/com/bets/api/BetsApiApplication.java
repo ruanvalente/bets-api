453e000c3cd1e59a2c6922968b35a0250b95b94c
ruanvalente <ruanopensuselinux@gmail.com>
2022-07-21 12:36:46 -0300

    chore: update mapstruct version

--- src/main/java/com/bets/api/BetsApiApplication.java
+++ src/main/java/com/bets/api/BetsApiApplication.java
@@ -2,12 +2,11 @@ package com.bets.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class BetsApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BetsApiApplication.class, args);
 	}
-
 }
