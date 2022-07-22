4822b60f955b6f460f129c0fa3b329e5b7e8d6fc
ruanvalente <ruanopensuselinux@gmail.com>
2022-07-22 20:53:02 -0300

    chore: close #3; add swagger

--- src/main/java/com/bets/api/resource/PunterResource.java
+++ src/main/java/com/bets/api/resource/PunterResource.java
@@ -5,6 +5,7 @@ import com.bets.api.dtos.PunterResponseDTO;
 import com.bets.api.entities.Punter;
 import com.bets.api.mappers.PunterMapper;
 import com.bets.api.resource.service.PunterResourceService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,12 +25,14 @@ public class PunterResource {
         this.punterMapper = punterMapper;
     }
 
-    @GetMapping
+    @GetMapping(produces = "application/json")
+    @ApiOperation(value = "Retorna todos os apostadores")
     public List<Punter> listAll() {
         return punterResourceService.listAll();
     }
 
-    @PostMapping
+    @PostMapping(produces = "application/json")
+    @ApiOperation(value = "Cria um(a) apostador")
     public ResponseEntity<PunterResponseDTO> createPunter(@Valid @RequestBody PunterRequestDTO punterRequestDTO) {
        PunterResponseDTO punter = punterResourceService.save(punterRequestDTO);
        return ResponseEntity.status(HttpStatus.CREATED).body(punter);
